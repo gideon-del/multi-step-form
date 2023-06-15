2de09b039951d08f040a11c74c8662640e2a4473
gideon-del <gidoeonchidi471@gmail.com>
2023-06-15 20:41:20 +0100

    Adding the colors and fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "./assets/fonts/Ubuntu-Bold.ttf";
+import "./assets/fonts/Ubuntu-Medium.ttf";
+import "./assets/fonts/Ubuntu-Regular.ttf";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
