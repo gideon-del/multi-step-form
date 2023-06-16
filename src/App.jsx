cfc33f5bcbaac0ac41aa599ffaee5dbd2bc2b731
gideon-del <gidoeonchidi471@gmail.com>
2023-06-16 07:15:47 +0100

    Fixing some toggle stuff

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import acrade from "./assets/images/icon-arcade.svg";
 import FirstStep from "./components/FirstStep";
 function App() {
   const [cuurentStep, setCurrentStep] = useState(1);
+  const [checked, setChecked] = useState(false);
   return (
     <>
-      <main className="font-Medium  lg:px-2 lg:py-2 m-auto flex flex-col md:flex-row  relative bg-white rounded-lg inset-0 max-w-7xl w-fit">
+      <main className="font-Medium  md:px-2 md:py-2 m-auto flex flex-col md:flex-row  relative bg-white rounded-lg inset-0 max-w-7xl w-fit">
         <section className="stack">
           <picture>
             <source srcSet={bgMob} media=" (max-width:768px) " />
@@ -46,38 +47,64 @@ function App() {
         </section>
         <section className=" flex-1 px-6 md:px-14 lg:px-28 md:pt-10 bg-ligthBlue md:bg-white">
           <section className="flex lg:max-w-3xl flex-col justify-between h-full">
-            {/* <div className=" md:h-full flex flex-col bg-white px-4 py-3 w-full rounded-lg relative -top-10">
+            <div className=" md:h-full flex flex-col bg-white px-4 py-3 w-full rounded-lg relative -top-10">
               <h1 className="font-Bold text-marineBlue text-xl md:text-4xl">
                 Select your Plan
               </h1>
               <p className="text-coolGray font-Regular md:mt-3 mt-1">
                 You have the option of monthly or yearly billing
               </p>
-              <form className="flex  md:gap-5 gap-3 md:mt-10 mt-3 ">
-                <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg">
-                  <img src={acrade} />
-                  <h1 className="flex flex-col gap-3 mt-9">
-                    {" "}
-                    Acrade <span>$9/mo</span>
-                  </h1>
+              <form className="flex flex-col md:gap-5 gap-3 md:mt-10 mt-3 ">
+                <div className="flex flex-row">
+                  <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg cursor-pointer">
+                    <img src={acrade} />
+                    <h1 className="flex flex-col gap-3 mt-9">
+                      {" "}
+                      Acrade <span>$9/mo</span>
+                    </h1>
+                  </div>
+                  <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg cursor-pointer">
+                    <img src={acrade} />
+                    <h1 className="flex flex-col gap-3 mt-9 cursor-pointer">
+                      {" "}
+                      Acrade <span>$9/mo</span>
+                    </h1>
+                  </div>
+                  <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg">
+                    <img src={acrade} />
+                    <h1 className="flex flex-col gap-3 mt-9">
+                      {" "}
+                      Acrade <span>$9/mo</span>
+                    </h1>
+                  </div>
                 </div>
-                <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg">
-                  <img src={acrade} />
-                  <h1 className="flex flex-col gap-3 mt-9">
-                    {" "}
-                    Acrade <span>$9/mo</span>
-                  </h1>
-                </div>
-                <div className="pr-20 pl-5 aspect-square border-coolGray border w-fit py-6 rounded-lg">
-                  <img src={acrade} />
-                  <h1 className="flex flex-col gap-3 mt-9">
-                    {" "}
-                    Acrade <span>$9/mo</span>
-                  </h1>
+                <div className="bg-ligthBlue py-3 px-3 flex flex-row">
+                  <p>Monthly</p>
+                  <div>
+                    <input
+                      type="checkbox"
+                      id="plan"
+                      name="plan"
+                      className="hidden"
+                    />
+                    <label
+                      className={`w-20 flex items-center   h-5 py-3 rounded-full bg-marineBlue ${
+                        checked ? "justify-end" : "justify-start"
+                      }`}
+                      onClick={() => setChecked((prev) => !prev)}
+                    >
+                      <div className={`translate-x-2/4 w-full`}>
+                        <span
+                          className={` inline-block h-5 transition-all duration-500 ease-in aspect-square  rounded-full my-1 bg-white`}
+                        ></span>
+                        <span></span>
+                      </div>
+                    </label>
+                  </div>
                 </div>
               </form>
-            </div> */}
-            <FirstStep />
+            </div>
+
             <div className=" flex justify-between  items-center">
               <button className="text-coolGray">Go Back</button>
               <button className="bg-marineBlue font-Bold text-white px-6 py-3 after:transition-all after:duration-300 after:ease-in-out relative rounded-md ml-auto  mt-3 mb-2 after:bg-white after:absolute after:inset-0 after:opacity-0 hover:after:opacity-40">
