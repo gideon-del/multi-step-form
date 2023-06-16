0ff4f1060469abf01e85a73c50b02501390a1835
gideon-del <gidoeonchidi471@gmail.com>
2023-06-16 05:39:14 +0100

    Done with first step

--- src/App.jsx
+++ src/App.jsx
@@ -32,9 +32,9 @@ function App() {
               className="w-full md:h-full object-cover   md:rounded-lg"
             />
           </picture>
-          <section className=" md:pt-10 flex gap-5 items-center justify-center md:flex-col md:justify-start md:gap-10 md:items-start">
+          <section className=" md:pt-10 flex gap-5 items-center justify-center md:flex-col md:justify-start md:gap-10 md:items-start md:pl-7">
             {steps.map((step, i) => (
-              <div className="flex gap-4  items-center justify-start ">
+              <div className="flex gap-4  items-center justify-start cursor-pointer">
                 <span
                   className={`${
                     i === cuurentStep
@@ -123,7 +123,58 @@ function App() {
             </div> */}
           </section>
         </section>
-        <section className="flex-1 bg-blue"></section>
+        <section className=" flex-1 px-10 md:px-14 lg:px-28 md:pt-10">
+          <div className="max-w-xl md:h-full flex flex-col">
+            <h1 className="font-Bold text-marineBlue text-xl md:text-4xl">
+              Personal info
+            </h1>
+            <p className="text-coolGray font-Regular mt-3">
+              Please provide your name, email address, and phone number
+            </p>
+            <form className="flex flex-col gap-5 mt-10 flex-1">
+              <div className="flex flex-col gap-3 ">
+                <label htmlFor="name" className="capitalize text-marineBlue">
+                  name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="e.g Stephen King"
+                  className="border border-coolGray max-w-xl px-4 py-2 rounded-lg focus-within:outline-none focus-within:border-0 focus-within:border-b focus-within:rounded-none"
+                />
+              </div>
+              <div className="flex flex-col  gap-3">
+                <label htmlFor="email" className="capitalize text-marineBlue">
+                  email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="e.g stephenking@lorem.com"
+                  className="border border-coolGray 
+                  px-4 py-2 rounded-lg focus-within:outline-none focus-within:border-0 focus-within:border-b focus-within:rounded-none"
+                />
+              </div>
+              <div className="flex flex-col  gap-3">
+                <label htmlFor="phone" className="capitalize text-marineBlue">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  placeholder="e.g +1 234 567 890"
+                  className="border border-coolGray max-w-xl px-4 py-2 rounded-lg focus-within:outline-none focus-within:border-0 focus-within:border-b focus-within:rounded-none"
+                />
+              </div>
+              <button className="bg-marineBlue font-Bold text-white px-4 py-2 rounded-md ml-auto mt-auto">
+                Next Step
+              </button>
+            </form>
+          </div>
+        </section>
       </main>
     </>
   );
