63cd407cf142bae6029efe72e1fc2d5947f34361
gideon-del <gideonchidi471@gmail.com>
2023-06-19 22:34:41 +0100

    Design fourth form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Steps from "./components/Steps";
 import Footer from "./components/Footer";
-import ThirdStep from "./components/ThirdStep";
 function App() {
   return (
     <>
@@ -8,7 +7,26 @@ function App() {
         <Steps />
         <section className=" flex-1 px-3 md:px-14 lg:px-18 md:pt-10 bg-ligthBlue md:bg-white w-fit">
           <section className="flex lg:max-w-3xl flex-col justify-between h-full">
-            <ThirdStep />
+            <div className=" md:h-full flex flex-col bg-white px-4 py-3 w-full rounded-lg relative -top-10">
+              <h1 className="font-Bold text-marineBlue text-xl md:text-4xl">
+                Finishing up
+              </h1>
+              <p className="text-coolGray font-Regular md:mt-3 mt-1">
+                Double check everything looks OK before confirming.
+              </p>
+              <div className="bg-ligthBlue">
+                <div className=" font-Bold text-marineBlue flex justify-between items-center border-b border-b-coolGray pb-4">
+                  <p className="flex flex-col">
+                    Arcade(Monthly){" "}
+                    <span className="underline text-purplishBlue font-Regular">
+                      Change
+                    </span>
+                  </p>
+                  <p className="">$9/mo</p>
+                </div>
+                <div></div>
+              </div>
+            </div>
 
             <Footer />
           </section>
