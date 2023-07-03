2ad13726ade10160d73103932d622f12d20cf79d
gideon-del <gideonchidi471@gmail.com>
2023-07-03 19:39:52 +0100

    Refactoring
    Doing cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Complete from "./components/Complete";
 
 function App() {
   const currentRef = useRef(null);
-  const { currentStep, error, steps, completed } = usePlan();
+  const { currentStep, error, completed } = usePlan();
   const submit = () => {
     if (!currentStep.validate) return;
     currentStep.validate(currentRef.current);
@@ -15,14 +15,10 @@ function App() {
   const CurForm = currentStep.component;
   return (
     <>
-      <main className="font-Medium  md:px-2 md:py-2 m-auto flex flex-col md:flex-row  relative bg-white rounded-lg inset-0 max-w-5xl flex-1">
+      <main className="font-Medium  md:px-2 md:py-2 m-auto flex flex-col md:flex-row  relative bg-white rounded-lg inset-0 max-w-5xl flex-1 min-h-screen md:min-h-none  ">
         <Steps />
-        <section className=" flex-1 px-3 md:px-14 lg:px-18 md:pt-10 bg-ligthBlue md:bg-white w-fit">
-          <div className="flex lg:max-w-3xl flex-col justify-between h-full">
-            {/* {currentStep.ref ? <CurForm ref={currentRef} /> : <CurForm />}
-            {error.trim().length !== 0 && (
-              <p className="text-center text-red-500 fotn-Bold">{error}</p>
-            )} */}
+        <section className=" flex-1 px-3 md:px-14 lg:px-18 md:pt-10 bg-ligthBlue md:bg-white w-fit flex">
+          <div className="flex lg:max-w-3xl flex-col justify-between  flex-1 ">
             {completed ? (
               <Complete />
             ) : (
